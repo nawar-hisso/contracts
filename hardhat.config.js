dc7ef22c1fff13c51d37ca4c82089f7c868f5982
Nawar Hisso <nawar@polytrade.finance>
2023-01-25 14:24:45 +0400

    :page_facing_up: Add Linters And Configs

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+const { default: CONSTANTS } = require('./configs/constants.js');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    mumbai: {
+      url: CONSTANTS.MUMBAI_RPC_URL,
+      accounts: [`0x${CONSTANTS.ACCOUNT_PRIVATE_KEY}`],
+    },
+    polygon: {
+      url: CONSTANTS.POLYGON_RPC_URL,
+      accounts: [`0x${CONSTANTS.ACCOUNT_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: CONSTANTS.MUMBAI_API_KEY,
+      polygon: CONSTANTS.MUMBAI_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: CONSTANTS.GAS_REPORTER,
+  },
+};
